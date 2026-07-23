e0f2dddb31b5b9e562b992185fd624ecb3dca835
zinxfx26-eng <zinxfx26@gmail.com>
2026-07-23 21:50:38 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("Prime Flix loaded successfully");
+
+
+// Future features:
+// - Supabase login
+// - Admin dashboard
+// - Upload movies
+// - Add news
+// - Edit website sections
+
+
+
+document.querySelector(".hero button")
+.onclick=function(){
+
+alert("Welcome to Prime Flix!");
+
+};
