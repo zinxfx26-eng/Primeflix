1de84607403e36b78020250ff20e39db5f1919b3
zinxfx26-eng <zinxfx26@gmail.com>
2026-07-24 15:10:49 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,6 @@
-const SUPABASE_URL = "https://zmzjfivgdovobjnpvbjy.supabase.co";
+<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>
+<script src="supabase.js"></script>
+<script src="script.js"></script>
 const SUPABASE_KEY = "sb_publishable_Jl4gi9s23jt3BdBFFwacmA_rqo7I5mJ";
 
 const supabase = window.supabase.createClient(
