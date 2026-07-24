1c281dd6d5d1fa26a7beb7e562469381c68f9c01
zinxfx26-eng <zinxfx26@gmail.com>
2026-07-24 15:00:31 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,8 +1,7 @@
-const SUPABASE_URL = "https://zmzjfivgdovobjnpvbjy.supabase.co
-";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InptempmaXZnZG92b2JqbnB2Ymp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ4MTk4NzQsImV4cCI6MjEwMDM5NTg3NH0.enTUAHJHQsaSXFG8BUuLEXpYwelpLHL9jnf3YeJ9SmA";
+const SUPABASE_URL = "https://zmzjfivgdovobjnpvbjy.supabase.co";
+const SUPABASE_KEY = "sb_publishable_Jl4gi9s23jt3BdBFFwacmA_rqo7I5mJ";
 
 const supabase = window.supabase.createClient(
   SUPABASE_URL,
-  SUPABASE_ANON_KEY
+  SUPABASE_KEY
 );
